8ec0e262b390bc0021016da80fbbc16c7f7fd272
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-01-26 03:03:50 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -18,11 +18,11 @@ class MusicLibraryController
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
 
-      input = gets.strip 
+      input = gets.strip
 
-      case input 
-      when "list songs" 
-        list_songs 
+      case input
+      when "list songs"
+        list_songs
       when "list artists"
         list_artists
       when "list genres"
@@ -32,9 +32,9 @@ class MusicLibraryController
       when "list genre"
         list_songs_by_genre
       when "play song"
-        play_song 
-      end 
-    end 
-      
+        play_song
+      end
+    end
+
 
 end
