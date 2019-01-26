c98a3603d5adda6819f7301724fc14c12b777d88
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-01-26 02:33:31 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,69 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@all = []
+
+  def initialize(name, artist=nil, genre=nil)
+    @name = name
+    self.artist = artist if artist
+    self.genre = genre if genre
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def self.create(name)
+    song = new(name)
+    song.save
+    song
+  end
+
+  def self.new_from_filename(filename)
+    split_name = filename.split(" - ")
+    artist_name = split_name[0]
+    song_name = split_name[1]
+    genre_name = split_name[2].gsub(".mp3","")
+
+
+    artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
+
+    new(song_name, artist, genre)
+  end
+
+  def self.create_from_filename(filename)
+    song = new_from_filename(filename)
+    song.save
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.add_song(self)
+  end
+
+  def self.find_by_name(name)
+    self.all.detect{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || Song.create(name)
+  end
+
+
+
+end
